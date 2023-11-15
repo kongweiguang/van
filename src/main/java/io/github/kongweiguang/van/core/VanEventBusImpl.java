c3dc5f123d80fbb1ec4e3ffebe6f6019bdcc4de2
kongweiguang <kongweiguang@icourt.cc>
2023-11-15 21:42:03 +0800

    add code

--- src/main/java/io/github/kongweiguang/van/core/VanEventBusImpl.java
+++ src/main/java/io/github/kongweiguang/van/core/VanEventBusImpl.java
@@ -1,18 +1,23 @@
 package io.github.kongweiguang.van.core;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public class VanEventBusImpl<C, R> implements VanEventBus<C, R> {
-    private final Router<C, R> router = new Router<>();
+    private final Map<Object, List<Handler<C, R>>> router = new HashMap<>();
 
     @Override
     public void push(final Msg<C, R> msg, final Consumer<R> call) {
         msg.callback(call);
-        router.get(msg.topic()).forEach(e -> e.handle(msg));
+        router.computeIfAbsent(msg.topic(), k -> new ArrayList<>()).forEach(e -> e.handle(msg));
     }
+
 
     @Override
     public void consumer(final Object topic, final Handler<C, R> handler) {
-        router.add(topic, handler);
+        router.computeIfAbsent(topic, k -> new ArrayList<>()).add(handler);
     }
 }
