0e34cb225338270cf0137279f431807a8e7b497b
kongweiguang <240524885@qq.com>
2023-11-17 23:29:01 +0800

    add code

--- src/main/java/io/github/kongweiguang/van/Van.java
+++ src/main/java/io/github/kongweiguang/van/Van.java
@@ -16,7 +16,7 @@ public final class Van {
         throw new UnsupportedOperationException("van must not be construct");
     }
 
-    private static final Map<String, Hub> hub_map = new ConcurrentHashMap<>();
+    private static final Map<String, Hub> hubs = new ConcurrentHashMap<>();
     private static final Hub hub = new HubImpl<>();
 
     /**
@@ -27,18 +27,18 @@ public final class Van {
      * @return hub
      */
     public static <C, R> Hub<C, R> hub() {
-        return hub;
+        return (Hub<C, R>) hub;
     }
 
     /**
      * 自定义hub
      *
-     * @param name hub的名字
+     * @param name hub的名称
      * @param <C>  操作的内容类型
      * @param <R>  返回的结果类型
      * @return hub
      */
     public static <C, R> Hub<C, R> hub(final String name) {
-        return hub_map.computeIfAbsent(name, k -> new HubImpl<>());
+        return (Hub<C, R>) hubs.computeIfAbsent(name, k -> new HubImpl<>());
     }
 }
