101711010c6687cf29518d11d95c9f17ff8b871b
kongweiguang <240524885@qq.com>
2023-11-18 23:10:30 +0800

    add code

--- src/test/java/io/github/kongweiguang/van/test/push_obj_method/MyHandler.java
+++ src/test/java/io/github/kongweiguang/van/test/push_obj_method/MyHandler.java
@@ -3,23 +3,21 @@ package io.github.kongweiguang.van.test.push_obj_method;
 import io.github.kongweiguang.van.core.Pull;
 import io.github.kongweiguang.van.test.metedata.User;
 
-import static jdk.nashorn.internal.objects.Global.println;
-
 public class MyHandler {
     @Pull
     public String fn(User user) {
-        println(user);
+        System.out.println(user);
         return "hello";
     }
 
     @Pull("bala")
     public String fn1() {
-        println("fn1");
+        System.out.println("fn1");
         return "hello1";
     }
 
     @Pull("bala")
     public void fn2() {
-        println("fn2");
+        System.out.println("fn2");
     }
 }
